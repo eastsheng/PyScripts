17c2c3adad336506b74f7b5ee29fafbd6e15a44f
eastsheng <eastsheng@hotmail.com>
2023-09-11 12:33:25 +0800

    modify outputTraj_N_Frame

--- scripts/outputTraj_N_Frame.py
+++ scripts/outputTraj_N_Frame.py
@@ -1,57 +1,13 @@
 #!/usr/bin/python
 import numpy as np
 import pandas as pd
+import ReadLammpsTraj as RLT
+
 class outputTraj(object):
 	"""docstring for outputTraj"""
 	def __init__(self, f):
 		super(outputTraj, self).__init__()
 		self.f = f
-	def read_info(self,):
-		with open(self.f,'r') as f:
-			L1 = f.readline()
-			L2 = f.readline()
-			L3 = f.readline()
-			L4 = f.readline()
-			L5 = f.readline()
-			L6 = f.readline()
-			L7 = f.readline()
-			L8 = f.readline()
-			L9 = f.readline().strip().split()[2:]#列标签
-			self.col = L9
-			step1 = int(L2)
-			self.atom_n = int(L4)
-			self.xlo,self.xhi = float(L6.split()[0]),float(L6.split()[1])
-			self.ylo,self.yhi = float(L7.split()[0]),float(L7.split()[1])
-			self.zlo,self.zhi = float(L8.split()[0]),float(L8.split()[1])
-			self.Lx = self.xhi-self.xlo
-			self.Ly = self.yhi-self.ylo
-			self.Lz = self.zhi-self.zlo
-			self.vlo = self.Lx*self.Ly*self.Lz
-			for i in range(self.atom_n+1):
-				Li = f.readline()
-				# print(Li)
-			step2 = int(f.readline())
-			self.step_inter = step2-step1
-			print("Step interval:",self.step_inter,"\nAtom number:",self.atom_n)
-			print("xlo:",self.xlo,"xhi:",self.xhi,"Lx:",self.Lx)
-			print("ylo:",self.ylo,"yhi:",self.yhi,"Ly:",self.Ly)
-			print("zlo:",self.zlo,"zhi:",self.zhi,"Lz:",self.Lz)
-		return self.step_inter,self.atom_n,self.Lx,self.Ly,self.Lz
-		
-	def read_traj(self,mframe,nframe,write_traj):
-		# 读取所有数据
-		# read_rows_0 = (self.atom_n+9)*(nframe-1)
-		read_rows_0 = (self.atom_n+9)*(mframe)
-		read_rows_1 = (self.atom_n+9)*(nframe+1)
-		# print(read_rows_0,read_rows_1,read_rows_1-read_rows_0)
-		with open(self.f,"r") as f, open(write_traj,"w") as traj:
-			for index, line in enumerate(f,1):
-				# print(index,"-----",read_rows_0,read_rows_1)
-				if index>read_rows_0 and index<=read_rows_1:
-					traj.write(line)
-					print(read_rows_0,"-----",index,"-----",read_rows_1)
-		return 
-
 
 	def read_xyz(self,mframe,nframe,write_xyz):
 		# 读取所有数据
@@ -71,31 +27,53 @@ class outputTraj(object):
 					print(read_rows_0,"-----",index,"-----",read_rows_1)
 		return 
 
+	def write_Ntraj(self,header,traj,write_traj):
+		with open(write_traj,"w") as w:
+			for h in range(len(header)):
+				w.write(header[h])
+			traj = traj.values
+			m,n = traj.shape
+			for i in range(m):
+				for j in range(n):
+					w.write(traj[i,j]+" ")
+				w.write("\n")
+		return 
+
+
+
 import sys
 
 if __name__ == '__main__':
-	#path = "./Interface/Steam3000_HeavyOil/473K/"
-	#f = path+"traj_out_1.lammpstrj"
-	#nframe = 1001
-    f = sys.argv[1]
-    mframe = int(sys.argv[2])
-    nframe = int(sys.argv[3])
-    wfile = sys.argv[4]
-    if wfile == "":
-        write_traj = "traj_"+str(mframe)+"-"+str(nframe)+".lammpstrj"
-        write_xyz = "traj_"+str(mframe)+"-"+str(nframe)+".xyz"
-    else:
-        write_traj = wfile
-        write_xyz = wfile
 
-    opt = outputTraj(f)
-    if f.split(".")[1]=="xyz":
-    	opt.read_xyz(mframe,nframe,write_xyz)
-    	print(write_xyz)
-    else:
-    	opt.read_info()
-    	opt.read_traj(mframe,nframe,write_traj)
-    	print(write_traj)
+	f = sys.argv[1]
+	mframe = int(sys.argv[2])
+	nframe = int(sys.argv[3])
+	wfile = sys.argv[4]
+	# path = "./"
+	# f = path+"traj_npt_dissociation_330_11.lammpstrj"
+	# mframe = 3001
+	# nframe = mframe
+	# wfile = ""
+	if wfile == "":
+		write_traj = "traj_"+str(mframe)+"-"+str(nframe)+".lammpstrj"
+		write_xyz = "traj_"+str(mframe)+"-"+str(nframe)+".xyz"
+	else:
+		write_traj = wfile
+		write_xyz = wfile
 
-    print("you ouput",mframe,"-",nframe,"th frame lammpstrj file")
-    print("Job done!!!")
+	opt = outputTraj(f)
+	md = RLT.ReadLammpsTraj(f)
+	md.read_info()
+	if f.split(".")[-1]=="xyz":
+		opt.read_xyz(mframe,nframe,write_xyz)
+		print(write_xyz)
+	elif f.split(".")[-1]=="lammpstrj":
+		header = md.read_header(mframe)
+		# print(header)
+		traj = md.read_traj(mframe)
+		opt.write_Ntraj(header,traj,write_traj)
+		# print(traj)
+
+	print(f)
+	print("your ouput",mframe,"-",nframe,"th frame lammpstrj file")
+	print("Job done!!!")

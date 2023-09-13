95605db315975abfbcef61a2ca643b71a3f253ba
eastsheng <eastsheng@hotmail.com>
2023-09-13 15:29:37 +0800

    modify output_N_traj

--- examples/lammpstrj/output_trj.sh
+++ examples/lammpstrj/output_trj.sh
@@ -1,9 +1,9 @@
 # compress the size of lammps dump file
 for((i=0;i<=100;i++));do
 echo $((j = $i * 100))
-outputTraj_N_Frame.py traj_npt_relax_287_1.lammpstrj $j $j traj_npt_relax_287_1_${j}.lammpstrj
+outputTraj_N_Frame.py traj_npt_relax_287_1.lammpstrj $j $j ./temp/traj_npt_relax_287_1_${j}.lammpstrj
 done
 
-cat *0.lammpstrj > traj_npt_relax_287_1_save.lammpstrj
+combine_files.py ./temp/ traj_npt_relax_287_1_save.lammpstrj
 
-rm  *0.lammpstrj
+rm -rf ./temp/
--- scripts/outputTraj_N_Frame.py
+++ scripts/outputTraj_N_Frame.py
@@ -13,7 +13,7 @@ class outputTraj(object):
 		# 读取所有数据
 		with open(self.f,"r") as f:
 			atom_n = int(f.readline())
-			print("Atom number =",atom_n)
+			# print("Atom number =",atom_n)
 
 		with open(self.f,"r") as f, open(write_xyz,"w") as traj:
 			# print(f.readline())
@@ -24,7 +24,7 @@ class outputTraj(object):
 				# print(index,"-----",line)
 				if index>=read_rows_0 and index<=read_rows_1:
 					traj.write(line)
-					print(read_rows_0,"-----",index,"-----",read_rows_1)
+					# print(read_rows_0,"-----",index,"-----",read_rows_1)
 		return 
 
 	def write_Ntraj(self,header,traj,write_traj):
@@ -45,35 +45,37 @@ import sys
 
 if __name__ == '__main__':
 
-	f = sys.argv[1]
-	mframe = int(sys.argv[2])
-	nframe = int(sys.argv[3])
-	wfile = sys.argv[4]
-	# path = "./"
-	# f = path+"traj_npt_dissociation_330_11.lammpstrj"
-	# mframe = 3001
-	# nframe = mframe
-	# wfile = ""
-	if wfile == "":
-		write_traj = "traj_"+str(mframe)+"-"+str(nframe)+".lammpstrj"
-		write_xyz = "traj_"+str(mframe)+"-"+str(nframe)+".xyz"
-	else:
-		write_traj = wfile
-		write_xyz = wfile
+	try:
+		f = sys.argv[1]
+	except:
+		f = "traj_npt_dissociation_330_11.lammpstrj"
+	try:	
+		mframe = int(sys.argv[2])
+		nframe = int(sys.argv[3])
+	except:
+		n = 0
+		mframe = n
+		nframe = n
+	try:
+		wfile = sys.argv[4]
+	except:
+		wfile = "traj_npt_dissociation_330_"+str(mframe)+"_"+str(nframe)+".lammpstrj"	
 
 	opt = outputTraj(f)
 	md = RLT.ReadLammpsTraj(f)
+	# md.__version__()
 	md.read_info()
 	if f.split(".")[-1]=="xyz":
-		opt.read_xyz(mframe,nframe,write_xyz)
-		print(write_xyz)
+		wfile = wfile.split(".lam")[0]+".xyz"
+		opt.read_xyz(mframe,nframe,wfile)
+		# print(write_xyz)
 	elif f.split(".")[-1]=="lammpstrj":
 		header = md.read_header(mframe)
 		# print(header)
 		traj = md.read_traj(mframe)
-		opt.write_Ntraj(header,traj,write_traj)
+		opt.write_Ntraj(header,traj,wfile)
 		# print(traj)
 
-	print(f)
-	print("your ouput",mframe,"-",nframe,"th frame lammpstrj file")
-	print("Job done!!!")
+	# print(f)
+	# print("your ouput",mframe,"-",nframe,"th frame lammpstrj file")
+	# print("Job done!!!")
